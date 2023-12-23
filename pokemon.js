37a1038afcb35f41aad3ec065e2ca745da18694b
Abd1io <154558921+Abd1io@users.noreply.github.com>
2023-12-23 14:48:54 +0300

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -8,9 +8,12 @@ form.addEventListener("submit", (event) => {
   const lineId = document.getElementById("lineId").value;
 
   fetch(`${API_URL.replace("{ids}", lineId)}`)
-    .then((response) => response.json())
+    .then((response) => {
+      // Parse the JSON response here using response.json()
+      return response.json();
+    })
     .then((lineStatusData) => {
-      // Code to handle the API response directly, without using statuses variable
+      // Code to handle the parsed data (now in JavaScript object format)
       if (lineStatusData.lineStatuses) {
         const firstStatusDescription = lineStatusData.lineStatuses[0].statusSeverityDescription;
         statusElement.textContent = `Line Status: ${firstStatusDescription}`;
@@ -19,7 +22,7 @@ form.addEventListener("submit", (event) => {
       }
     })
     .catch((error) => {
-      // Handle any errors thatoccur during the fetch or response parsing
+      // Handle any errors that occur during the fetch or response parsing
       console.error("Error fetching line status:", error);
       statusElement.textContent = "An error occurred while retrieving line status.";
     });
