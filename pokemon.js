1c4cb0699c35f33a442da2ef6ce60f7b4d9c12b3
Abd1io <154558921+Abd1io@users.noreply.github.com>
2023-12-23 13:42:56 +0300

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,29 +1,21 @@
 const API_URL = "https://api.tfl.gov.uk/Line/{ids}/Status";
-const statusElement = document.getElementById("lineStatus");
+const statusElement = document.getElementById("lineStatus"); // Target the correct element
 const form = document.getElementById("lineStatusForm");
-form.addEventListener("submit", (event) => {
-  // Prevents the form from submitting and refreshing the page
-  event.preventDefault();
-  const lineId = document.getElementById("lineId").value;
-fetch(`${API_URL.replace("{ids}", lineId)}`)
-    .then((response) => response.json())
-    .then((lineId) => {
-      // create elements for the Pokemon Card
-      //const div = document.createElement("div");
-      //const image = document.createElement("img");
-      //const name = document.createElement("h1");
-      //div.className = "card";
-     // image.src = newPokemon.sprites.front_default; // Use the available property;
-     // name.textContent = newPokemon.name;
-      //div.appendChild(name);
-     // div.appendChild(image);
-     // root.appendChild(div);
 
-      // another image option:
-      //image.src = newPokemon.sprites.front_default;
-      // other ideas:
-      // div.appendChild(pokemonNameLabel);
-      // div.appendChild(pokemonAttack);
-      //div.appendChild(pokemonHealth);
-  });
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const lineId = document.getElementById("lineId").value;
+
+  fetch(`${API_URL.replace("{ids}", lineId)}`)
+    .then((response) => response.json())
+    .then((lineStatusData) => {
+      // Access and display line status information
+      const statusSeverityDescription = lineStatusData.lineStatuses[0].statusSeverityDescription;
+      statusElement.textContent = `Line Status: ${statusSeverityDescription}`;
+    })
+    .catch((error) => {
+      console.error("Error fetching line status:", error);
+      statusElement.textContent = "Error fetching line status."; // Display error message
+    });
 });
