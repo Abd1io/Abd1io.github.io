5b45f3a00acabc599f1302b93c6324a50a8880e6
Abd1io <154558921+Abd1io@users.noreply.github.com>
2023-12-24 16:14:39 +0300

    Create App2.js

---
+++ New2/App2.js
@@ -0,0 +1,41 @@
+$(() => {
+  // Get all line IDs
+  $.get('https://api.tfl.gov.uk/Line/Meta/Modes')
+    .done(data => {
+      const lineIds = data.map(line => line.id);
+
+      // Loop through each line and fetch status
+      lineIds.forEach(lineId => {
+        $.get(`https://api.tfl.gov.uk/Line/${lineId}/Status`)
+          .done(data => {
+            const lineName = data.lineStatuses[0].name;
+            const statusDescription = data.lineStatuses[0].statusSeverityDescription;
+            const statusColor = getStatusColor(statusDescription);
+
+            // Create bar element
+            const bar = document.createElement('div');
+            bar.classList.add('line-status-bar');
+            bar.style.backgroundColor = statusColor;
+            bar.textContent = `${lineName}: ${statusDescription}`;
+
+            // Add bar to HTML
+            const lineStatusesContainer = document.getElementById('line-statuses');
+            lineStatusesContainer.appendChild(bar);
+          });
+      });
+    });
+});
+
+// Define function to map status to color
+function getStatusColor(statusDescription) {
+  switch (statusDescription) {
+    case 'Good Service':
+      return 'green';
+    case 'Minor Delays':
+      return 'yellow';
+    case 'Severe Delays':
+      return 'red';
+    default:
+      return 'gray';
+  }
+}
