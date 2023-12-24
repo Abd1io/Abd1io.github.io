e72453834d3a782b9d8f3963911fb59c1529236a
Abd1io <154558921+Abd1io@users.noreply.github.com>
2023-12-24 17:25:56 +0300

    console log add

--- New2/App2.js
+++ New2/App2.js
@@ -1,19 +1,29 @@
 $(() => {
+  console.log('Beginning data retrieval...');
+
   function getData() {
+    console.log('Fetching tube line data...');
     $.get('https://api.tfl.gov.uk/Line/Mode/tube')
       .done(data => {
+        console.log('Tube line data received:', data);
+
         const lineIds = data.map(line => line.id);
 
         // Loop through each line ID and fetch status
         lineIds.forEach(lineId => {
+          console.log(`Fetching status for line ID ${lineId}...`);
           $.get(`https://api.tfl.gov.uk/Line/${lineId}/Status`)
             .done(data => {
+              console.log('Status data for line:', data);
+
               const lineName = line.name;
               const statusDescription = line.statusSeverityDescription;
 
-              console.log({ lineName, statusDescription }); // Log data for HTML handling (excluding color)
+              console.log(`Line ${lineName}: ${statusDescription}`); // Log data for HTML handling
             });
         });
       });
   }
+
+  getData(); // Trigger the data fetching process
 });
