89872502434ddf736323afa3c76b77c026dce3c4
Abd1io <154558921+Abd1io@users.noreply.github.com>
2023-12-26 11:32:20 +0300

    h

--- New2/App2.js
+++ New2/App2.js
@@ -1,22 +1,39 @@
-$(() => {
+document.addEventListener('DOMContentLoaded', () => {
+  // Function to fetch data from the API
   function getData() {
-    $.ajax({
-      url: 'https://api.tfl.gov.uk/Line/Mode/tube/Status',
-      method: 'GET',
-      success: function(data) {
+    // Fetch data from the API
+    fetch('https://api.tfl.gov.uk/Line/Mode/tube/Status')
+      .then(response => {
+        // Check if the response is ok, else throw an error
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse response to JSON
+      })
+      .then(data => {
+        // Iterate through the received data
         data.forEach(line => {
+          // Extract line name and statusSeverityDescription from the response
           let lineName = line.name;
-          let status = line.statusSeverityDescription;
-          appendToTable(lineName, status);
+          let status = line.lineStatuses[0].statusSeverityDescription; // Access statusSeverityDescription from lineStatuses array
+          appendToTable(lineName, status); // Append data to the table
         });
-      }
-    });
+      })
+      .catch(error => {
+        // Handle any errors that occurred during fetch
+        console.error('There has been a problem with your fetch operation:', error);
+      });
   }
 
+  // Function to append data to the table
   function appendToTable(lineName, status) {
+    // Create a new row with lineName and status
     let newRow = `<tr><td>${lineName}</td><td>${status}</td></tr>`;
-    $('#lineStatusBody').append(newRow);
-  } 
+    // Append the new row to the existing table body
+    document.getElementById('lineStatusBody').innerHTML += newRow;
+  }
 
+  // Call the function to fetch and display data
   getData();
 });
+
